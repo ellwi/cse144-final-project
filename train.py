74ee6b538a6354b97088c6ac3cba306da509c1d2
eewilson <elinaewilson@gmail.com>
2026-05-18 14:31:23 -0700

    moved sample collection outside of Dataset class; split dataset

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 from PIL import Image
+from sklearn.model_selection import train_test_split
 
 # =========================================
 # Data preprocessing/Setup
@@ -19,30 +20,41 @@ from PIL import Image
 # custom Dataset class to make sure that samples have correct labels
 # https://blog.roboflow.com/pytorch-custom-dataset/
 # https://docs.pytorch.org/tutorials/beginner/basics/data_tutorial.html
-class CSE144Dataset(Dataset):
 
-    def __init__(self, root_dir, transform=None, resize=None):
-        self.root_dir = root_dir
-        self.transform = transform 
-        self.resize = resize
-
-        self.samples = []
-        # go through all folders 0-99
-        for folder_name in os.listdir(root_dir):
+def collect_samples(root_dir):
+    """
+    Grabs the images and folder labels from root and creates a list of each
+    """
+    images = []
+    labels = []
+    for folder_name in os.listdir(root_dir):
             label = int(folder_name)
             folder_path = os.path.join(root_dir, folder_name)
             # go through all images in folder
             for image_name in os.listdir(folder_path):
-                # add tuple of image path and label to samples
-                self.samples.append((os.path.join(folder_path, image_name), label))
+                images.append(os.path.join(folder_path, image_name))
+                labels.append(label)
+    return images, labels
+
+
+class CSE144Dataset(Dataset):
+    """
+    Defines a Dataset class, needed for pytorch to handle data loading.
+    Applies any needed transforms to images.
+    """
+    def __init__(self, images, labels, transform=None, resize=None):
+        self.images = images
+        self.labels = labels
+        self.transform = transform 
+        self.resize = resize
         
         # TODO add data augmentation
 
     def __len__(self):
-        return len(self.samples) # should be 1079
+        return len(self.images) # should be 1079
     
     def __getitem__(self, idx):
-        path, label = self.samples[idx] 
+        path = self.images[idx] 
         image = Image.open(path).convert("RGB")
 
         # resize
@@ -50,14 +62,14 @@ class CSE144Dataset(Dataset):
             new_h, new_w = self.resize
             image = transforms.functional.resize(image, (new_h, new_w))
         
-        # transform (into tensor)
+        # transform 
         if self.transform:
             image = self.transform(image)
 
-        return image, label
+        return image, self.labels[idx]
 
 
-def dataset_unittest():
+def dataset_unittest(dataset):
     
     print(f"Dataset size: {len(dataset)}")
     # Grab a single sample
@@ -72,6 +84,7 @@ def dataset_unittest():
 
     # Sanity checks
     assert image.shape[0] == 3, "Expected 3 channels (RGB)"
+    num_classes = 100
     assert label in range(num_classes), f"Label {label} out of range"
 
     # print out some images and their labels to visually confirm
@@ -89,21 +102,32 @@ def dataset_unittest():
 
 # We'll use this directory on my pc for the actual full-scale training
 
-# create dataset instance
-transform = transforms.ToTensor()
-dataset = CSE144Dataset(r"C:\Users\eewilson\Documents\University\CSE144\finalproject_data\train", transform=transform, resize=(224, 224))
-num_classes = 100
-
-dataset_unittest()
-
-
+# collect lists of images and labels from directory 
+images, lables = collect_samples(r"C:\Users\eewilson\Documents\University\CSE144\finalproject_data\train")
 
 
 # split dataset into train and validate
+# https://www.geeksforgeeks.org/deep-learning/how-to-split-a-dataset-using-pytorch/
 
-#TODO: https://www.geeksforgeeks.org/deep-learning/how-to-split-a-dataset-using-pytorch/
+train_images, val_images, train_labels, val_labels = train_test_split(
+    images, lables, test_size=0.2, stratify=lables, random_state=7
+)
+print("Number of training samples:", len(train_images))
+print("Number of validation samples:", len(val_images))
 
+# define transforms for training and validation data
+train_transform = transforms.Compose([
+    transforms.ToTensor()
+    ])
+val_transform = transforms.Compose([
+    transforms.ToTensor()
+    ])
 
+train_dataset = CSE144Dataset(train_images, train_labels, transform=train_transform, resize=[224, 224])
+val_dataset = CSE144Dataset(val_images, val_labels, transform=val_transform, resize=[224, 224])
+
+#dataset_unittest(train_dataset)
+#dataset_unittest(val_dataset)
 
 
 
